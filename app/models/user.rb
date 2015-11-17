ed7a90df77455a81d4e684e9f68d6ba2aae0c9d8
Damon <damon.kenul@gmail.com>
2015-11-17 09:50:18 -0500

    Initialize project

---
+++ app/models/user.rb
@@ -0,0 +1,39 @@
+class User < ActiveRecord::Base
+
+  after_initialize :ensure_session_token
+
+  validates :username, :password_digest, :session_token, presence: true
+  validates :password, length: { minimum: 6, allow_nil: true }
+
+  has_many :posts
+
+  attr_reader :password
+
+  def self.find_by_credentials(username, password)
+    user = User.find_by(username: username)
+    return nil unless user && user.is_password?(password)
+    user
+  end
+
+  def password=(pass)
+    @password = pass
+    self.password_digest = BCrypt::Password.create(pass)
+  end
+
+  def is_password?(pass)
+    BCrypt::Password.new(password_digest).is_password?(pass)
+  end
+
+  def reset_session_token!
+    self.session_token = SecureRandom.urlsafe_base64(16)
+    self.save!
+    self.session_token
+  end
+
+  private
+
+  def ensure_session_token
+    self.session_token ||= SecureRandom.urlsafe_base64(16)
+  end
+
+end
