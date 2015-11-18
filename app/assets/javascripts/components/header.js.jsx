4cc9738ab9a2f8021cfe51582df652bb54b6a32f
Damon <damon.kenul@gmail.com>
2015-11-18 13:25:36 -0500

    Add placeholder icons

--- app/assets/javascripts/components/header.js.jsx
+++ app/assets/javascripts/components/header.js.jsx
@@ -3,7 +3,7 @@ var Header = React.createClass ({
     return (
       <div className="header-container">
         <header className="header group">
-          HELLO FROM THE HEADER - BY THE WAY SIGN IN AND SIGN UP BUTTONS DONT WORK WAITING FOR REACT AUTH LECTURE
+          HELLO FROM THE HEADER
         </header>
       </div>
     );
--- app/assets/javascripts/components/nav_bar.js.jsx
+++ app/assets/javascripts/components/nav_bar.js.jsx
@@ -4,8 +4,8 @@ var NavBar = React.createClass ({
       <div className="nav-container">
         <nav className="nav group">
           <ul className="left-nav group">
-            <li>HackExchange</li>
-            <li>Mail</li>
+            <li><img className="logo-icon" src='assets/HackExchange-logo.png' /></li>
+            <li><img className="mail-icon" src='assets/mail.png' /></li>
           </ul>
           <ul className="right-nav group">
             <li>Sign In</li>
