d0e785febb793cdeb8b6fe6440a0e45b2c1c65e1
Damon <damon.kenul@gmail.com>
2015-11-18 18:09:51 -0500

    Add Logo dropdown component

---
+++ app/assets/javascripts/components/logo-dropdown.js.jsx
@@ -0,0 +1,22 @@
+var LogoDropdown = React.createClass ({
+
+  render: function() {
+
+    return (
+      <div>
+        <header><h2>Current Community</h2></header>
+        <div className="blueline"><p>logo is a placeholder</p>
+        <p>needs to be a sprite</p>
+        <p>so is this pink color :)</p><p>bla</p></div>
+        <header><h2>Your Communities</h2></header>
+        <div><p>bla</p><p>bla</p></div>
+        <header><h2>All Communities</h2></header>
+        <div><p>bla</p><p>bla</p><p>bla</p><p>bla</p>
+        <p>bla</p><p>bla</p><p>bla</p><p>bla</p>
+        <p>bla</p><p>bla</p><p>bla</p><p>bla</p></div>
+
+      </div>
+    );
+  }
+
+});
--- app/assets/javascripts/components/nav_bar.js.jsx
+++ app/assets/javascripts/components/nav_bar.js.jsx
@@ -1,17 +1,50 @@
 var NavBar = React.createClass ({
+
+  getInitialState: function() {
+    return {logoClicked: false};
+  },
+
+  handleLogoClick: function(event) {
+    if (this.state.logoClicked) {
+      event.currentTarget.className = "";
+      this.logoInjection = undefined;
+      this.setState({logoClicked: false});
+    } else {
+      event.currentTarget.className = "clicked";
+      this.logoInjection =
+        <div className="logo-dropdown" onClick={this.propagationCanceller}>
+          <LogoDropdown />
+        </div>;
+      this.setState({logoClicked: true});
+    }
+  },
+
+  propagationCanceller: function(event) {
+    event.stopPropagation();
+  },
+
+
   render: function() {
     return(
       <div className="nav-container">
         <nav className="nav group">
+
           <ul className="left-nav group">
-            <li><img className="logo-icon" src={window.images.logoIcon} /></li>
+            <li onClick={this.handleLogoClick}>
+              <img className="logo-icon" src={window.images.logoIcon} />
+              {this.logoInjection}
+            </li>
+
             <li><img className="mail-icon" src={window.images.mailIcon} /></li>
           </ul>
+
           <ul className="right-nav group">
             <li>Sign In</li>
             <li>Sign Up</li>
           </ul>
+
         </nav>
+
       </div>
     );
   }
