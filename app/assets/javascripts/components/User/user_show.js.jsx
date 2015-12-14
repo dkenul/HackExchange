ce29c84d10f717d8c90255c6b8c5da05f12ff1eb
Damon <damon.kenul@gmail.com>
2015-12-14 16:20:48 -0500

    separate css. build profile further

--- app/assets/javascripts/components/User/user_show.js.jsx
+++ app/assets/javascripts/components/User/user_show.js.jsx
@@ -1,10 +1,10 @@
-// this will have the appropriate user_id prop based on the clicked user
-
-
 var userShow = React.createClass ({
 
   getInitialState: function() {
-    return {user: UsersStore.findUserById(parseInt(this.props.params.user_id))}
+    return {
+      user: UsersStore.findUserById(parseInt(this.props.params.user_id)),
+      currentClicked: "communities"
+    }
   },
 
   componentDidMount: function() {
@@ -21,10 +21,51 @@ var userShow = React.createClass ({
   },
 
   render: function() {
-    debugger;
+
+
+
+    var username;
+    var bottomSectionContent;
+    if (this.state.user) {
+      username = this.state.user.username;
+
+      if (this.state.currentClicked == "communities") {
+        bottomSectionContent = this.state.user.communities.map(function(community) {
+          return (
+            <div className="community-item-container" key={community.id}>
+              <a href={"#/communities/" + community.id} className="community-item group">
+                <img src="http://placecorgi.com/25/25" className="community-item-thumb" />
+                <div className="text-container">
+                  <div className="community-name">{community.name}</div>
+                  <div className="community-description">{community.description}</div>
+                </div>
+              </a>
+            </div>
+          );
+        });
+      }
+    }
+
     return (
-      <div>
-        Hey
+      <div className="profile-container">
+        <div className="top-section">
+          <div className="username">
+            {username}
+          </div>
+        </div>
+
+        <div className="bottom-section">
+          <ul className="bottom-section-nav group" key="b-s-n">
+            <li>Questions</li>
+            <li>Answers</li>
+            <li>Reputation</li>
+            <li>Communities</li>
+          </ul>
+
+          <div className="bottom-section-content">
+            {bottomSectionContent}
+          </div>
+        </div>
       </div>
     );
   }
