6f3ca0fbd6b7197c25318bcfe300db4e59a17d10
Damon <damon.kenul@gmail.com>
2015-12-17 02:04:51 -0500

    change footer

--- app/assets/javascripts/components/Peripherals/footer.js.jsx
+++ app/assets/javascripts/components/Peripherals/footer.js.jsx
@@ -3,10 +3,8 @@ var Footer = React.createClass ({
     return (
       <div className="footer-container">
         <header className="footer group">
-          <li>HELLO FROM THE FOOTER</li>
-          <li>WOW ITEMS</li>
-          <li>COOL</li>
-          <li>SO MANY THINGS WILL GO HERE</li>
+          <li>This site is inspired by <a href="http://www.stackexchange.com">Stack Exchange</a></li>
+          <li>Feel free to check out my other projects on <a href="http://www.github.com/dkenul">GitHub</a></li>
         </header>
       </div>
     );
